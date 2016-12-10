738bee46f8eef309f713dfdeaedb0c3839f6f45d
Shweta Stha <swetacresta30@gmail.com>
2016-12-10 10:19:41 +0000

    Post comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          has_many :posts
          mount_uploader :photo , PhotoUploader
+         has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  resource :posts
+  resource :posts do
+   resource :comments
+ end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
