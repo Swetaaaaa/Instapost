d7d9a442981cefb12ada5697fa29a3f412562e98
Shweta Stha <swetacresta30@gmail.com>
2016-12-13 11:50:03 +0000

    User info

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resource :posts do
    resource :comments
  end
+ resource :users,only: :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
